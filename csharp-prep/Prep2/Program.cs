475cb4c2d8fe37878b034f8ec55a46c855446fa2
wjdavie <wesley.davie@gmail.com>
2023-09-16 21:45:02 -0400

    Completed Prep 1 and Prep 2 assignments.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percent = Console.ReadLine();
+        int grade = int.Parse(percent);
+
+        string letter = "";
+
+        if (grade > 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter} ");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed! ");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass. Keep studying! ");
+        }
     }
 }
